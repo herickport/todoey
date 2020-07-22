d35c25ea75acc148f19997784eeaf1ce83afb43b
Herick Portugues <herick.portugues@gmail.com>
2020-07-22 11:25:47 -0300

    Create Project

---
+++ android/app/src/main/kotlin/br/com/portugues/todoey/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.portugues.todoey
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todoey/screens/tasks_screen.dart';
+
+void main() => runApp(Todoey());
+
+class Todoey extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(
+      title: 'Todoey',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.lightBlueAccent,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TasksScreen(),
+    );
+  }
+}
