4548f01be92f4b4215796c2f6a2b2c8478331379
Herick Portugues <herick.portugues@gmail.com>
2020-07-22 15:24:29 -0300

    Remove Glow when Scroll

--- lib/components/tasks_list.dart
+++ lib/components/tasks_list.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/components/task_tile.dart';
+import 'package:todoey/utils/no_glow_behavior.dart';
 
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      // padding: EdgeInsets.symmetric(
-      //   horizontal: 32.0,
-      //   vertical: 16.0,
-      // ),
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ScrollConfiguration(
+      behavior: NoGlowBehavior(),
+      child: ListView(
+        children: <Widget>[
+          TaskTile(),
+          TaskTile(),
+          TaskTile(),
+        ],
+      ),
     );
   }
 }
